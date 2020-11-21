6b3c0fdf99b1ead16180dafff7a3c707d3226274
suelytohm <suelytohm.02@gmail.com>
2020-11-21 09:19:47 -0300

    first commit

---
+++ aula3/exercicio.js
@@ -0,0 +1,4 @@
+
+console.log('Meu nome é "Suelytohm". Estou aprendendo Javascript às ' + 10 + ' da manhã.');
+
+console.log('Meu nome é "Suelytohm". Estou aprendendo Javascript às', 10 , 'da manhã.');
---
+++ aula3/index.js
@@ -0,0 +1,6 @@
+console.log("Suelytohm 'Oliveira'");
+console.log('Suelytohm "Oliveira"');
+console.log(`Suelytohm Oliveira`);
+
+
+console.log(123, 44.13, "Suelytohm Oliveira");
