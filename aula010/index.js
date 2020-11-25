5833e79feda9bcb4595206256f67bbc21f34e1eb
suelytohm <suelytohm.02@gmail.com>
2020-11-25 18:06:28 -0300

    Operadores Aritméticos

---
+++ aula010/index.js
@@ -0,0 +1,38 @@
+/**
+ * Operadores aritméticos
+ * + Adição / Concatenação (- * /)
+ * ** Potenciação
+ * % Resto de divisão
+ */
+
+let contador = 1;
+/**
+ * Atribui o valor após a execução
+ * contador++
+ *  */ 
+console.log(contador++); // 1
+
+/**
+ * Atribui o valor antes da execução
+ * ++contador
+ *  */ 
+
+console.log(++contador); // 3
+
+/**
+ * Diminui o valor após a execução
+ * contador--
+ *  */ 
+console.log(contador--); // 3
+
+/**
+ * Diminui o valor antes da execução
+ * --contador
+ *  */ 
+console.log(--contador); // 1
+
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+
+console.log((num1 + num2) * num3); // 70
