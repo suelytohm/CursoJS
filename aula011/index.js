17ed66d16c04429de251266c15b67017317aea7f
suelytohm <suelytohm.02@gmail.com>
2020-12-01 22:55:42 -0300

    prompt, alert e number

--- aula011/index.js
+++ aula011/index.js
@@ -1,3 +1,9 @@
-let n1 = prompt("Digite um número:");
+let n1 = prompt("Digite o primeiro número:");
+let n2 = prompt("Digite o segundo número:");
 
-alert('Você digitou ' + n1);
+
+n1 = Number(n1);
+n2 = Number(n2);
+const soma = n1 + n2;
+
+alert('A soma é: ' + soma);
